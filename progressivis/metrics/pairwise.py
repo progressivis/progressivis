8a17d67e07602130d4b3150710281599905cbb76
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-08-26 17:41:17 -0400

    starting to test pairwise

--- progressivis/metrics/pairwise.py
+++ progressivis/metrics/pairwise.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 """ Computes the distance matrix from each row of a data frame.
-
-The matrix is kept as a `condensed distance matrix` form.
-See http://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.squareform.html
-
 """
 from progressivis.core.common import ProgressiveError
 from progressivis.core.dataframe import DataFrameModule
@@ -11,32 +7,28 @@ from progressivis.core.slot import SlotDescriptor
 
 import numpy as np
 import pandas as pd
-from spipy.spatial.distance import pdist
-from sklean.metrics.pairwise import _VALID_METRICS, pairwise_distances
+from scipy.spatial.distance import pdist
+from sklearn.metrics.pairwise import _VALID_METRICS, pairwise_distances
 
 import logging
 logger = logging.getLogger(__name__)
 
 class PairwiseDistances(DataFrameModule):
-    schema = [('distance', np.dtype(float), np.nan),
-              DataFrameModule.UPDATE_COLUMN_DESC]
-
     def __init__(self, metric='euclidean', columns=None, n_jobs=1, **kwds):
         self._add_slots(kwds,'input_descriptors',
                         [SlotDescriptor('df', type=pd.DataFrame)])
-        super(PairedDistances, self).__init__(**kwds)
+        super(PairwiseDistances, self).__init__(**kwds)
         self._metric = metric
         self._n_jobs = n_jobs
         if (metric not in _VALID_METRICS and
             not callable(metric) and metric != "precomputed"):
             raise ProgressiveError('Unknown distance %s', metric)
         self.columns = columns
-        self._df = self.create_dataframe(PairedDistances.schema)
 
     def is_ready(self):
         if not (self.get_input_slot('df').is_buffer_empty()):
             return True
-        return super(PairedDistances, self).is_ready()
+        return super(PairwiseDistances, self).is_ready()
 
     def run_step(self,run_number,step_size,howlong):
         dfslot = self.get_input_slot('df')
@@ -65,11 +57,30 @@ class PairwiseDistances(DataFrameModule):
 
         # We have the old matrix Si of size (n), we want to complete it with
         # two sub matrices, Sij and Sj of length (m).
-        # We are given a "budget" of step_size (s)operations
-        # See how many new rows fit. These will be the (j) new rows
+        # We are given a "budget" of step_size (s) operations
+        # See how many new rows we can compute with our budget.
+        # These will be the (j) new rows
+        # We need to comput Sij, which will take n*m operations
+        # and Sj which will take m*m, so we search m for n*m + m*m = s
+        # m^2 + n*m -s = 0, a=1, b=n, c=-s, solution is -b +- sqrt(b^2-4ac)/2a
+        # The only positive solution is -n + sqrt(n^2+4s) / 2
 
-        m = (-n + math.sqrt(n*n + 4*step_size)) / 2
+        m = (-n + np.sqrt(n*n + 4*step_size)) / 2.0
+        m = np.min([1.0, m])
 
+        Si = self._df
         indices = dfslot.next_buffered(m)
         j = rows[indices]
         Sj = pairwise_distances(j, metric=self.metric, n_jobs=self.n_jobs)
+        if Si is None:
+            S = Sj
+            index = Si.index
+        else:
+            i = df[Si.index]
+            Sij = pairwise_distances(i,j)
+            Sji = Sij.T
+            S1 = np.hstack((Si, Sij))
+            S2 = np.hstack((Sji, Sj))
+            S = np.vstack((S1, S2))
+            index = Si.index + df.index[indices]
+        self._df = pd.DataFrame(S,index=index)
---
+++ tests/test_pairwise.py
@@ -0,0 +1,22 @@
+import unittest
+
+from progressivis import *
+from progressivis.io import VECLoader
+from progressivis.metrics import PairwiseDistances
+from progressivis.datasets import get_dataset
+
+def print_len(x):
+    if x is not None:
+        print len(x)
+
+class TestPairwiseDistances(unittest.TestCase):
+    def test_pairwise_distances(self):
+        vec=VECLoader(get_dataset('warlogs'))
+        dis=PairwiseDistances(metric='cosine')
+        dis.input.df = vec.output.df
+        cnt = Every(proc=print_len,constant_time=True)
+        cnt.input.inp = dis.output.df
+        vec.start()
+
+if __name__ == '__main__':
+    unittest.main()
