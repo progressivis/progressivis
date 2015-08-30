7519f1195403ce64e6da94b5501d47c9d9dbc926
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-08-30 16:31:34 -0400

    continue mds

--- progressivis/manifold/mds.py
+++ progressivis/manifold/mds.py
@@ -15,10 +15,16 @@ class MDS(DataFrameModule):
               ('y', np.dtype(float), np.nan),
               DataFrameModule.UPDATE_COLUMN_DESC]              
 
+    schema = [('max_iter', np.dtype(int), 300),
+              ('n_init', np.dtype(int), 8),
+              ('eps', np.dtype(float), 1e-3),
+              DataFrameModule.UPDATE_COLUMN_DESC]              
+
     def __init__(self, n_jobs=1, **kwds):
         self._add_slots(kwds,'input_descriptors',
                         [SlotDescriptor('distance', type=pd.DataFrame)])
         super(MDS, self).__init__(dataframe_slot='mds', **kwds)
+        self._step_size = 100
         self._n_jobs = n_jobs
         self._df = None
         self._cache = None
@@ -32,10 +38,36 @@ class MDS(DataFrameModule):
         dfslot = self.get_input_slot('distance')
         df = dfslot.data()
         dfslot.update(run_number, df)
-        if len(dfslot.deleted) or len(dfslot.updated) > len(dfslot.created):
-            dfslot.reset()
-            logger.info('Reseting history because of changes in the input df')
-        dfslot.buffer_created()
-        
+        p = self.parameter
+        n = len(df)
+
+        if self._df is None:
+            mds = MDS(2, max_iter=p.max_iter, dissimilarity='precomputed', n_init=p.n_init)
+            if step_size >= n:
+                index=df.index
+            else:
+                index=df.index[:step_size]
+            S = df.loc[index,index]
+            Y, stress = mds.fit_transform(S)
+            # Ignore stress for now
+            self._df = pd.DataFrame(Y,columns=['x', 'y'], index=index)
+            self._df[self.UPDATE_COLUMN] = run_number
+        else:
+            m = len(self._df)
+            if (m+step_size) >= n:
+                index=df.index
+            else:
+                index=df.index[:m+step_size]
+            S = df.loc[index,index]
+            new_points = self.barycenter(self.df_, S, index)
+            new_df = df.DataFrame(new_points, index=df.index[m:step_size])
+            new_df[self.UPDATE_COLUMN] = run_number
+            self.df_ = self._df.concat(new_df) # don't ignore index
+            mds = MDS(2, max_iter=p.max_iter, dissimilarity="precomputed", n_init=1)
+            Y, stress = mds.fit_transform(S, init=self.df_[['x','y']])
+            self._df['x'] = Y[0]
+            self._df['y'] = Y[1]
+        next_state = state_ready if (len(self._df) < n) else state_blocked
+        return self._return_run_step(next_state,
+                                     steps_run=len(index), reads=len(index), updates=len(self._df))
             
-    
