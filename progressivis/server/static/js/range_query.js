f3bc290031b16f9a1988198e9ed62f824f4437ce
Romain Primet <romain.primet@inria.fr>
2015-11-25 10:00:05 +0100

    update module page

--- progressivis/server/static/js/range_query.js
+++ progressivis/server/static/js/range_query.js
@@ -1,3 +1,24 @@
-function range_query_ready(){
-  module_ready();
-}
+var range_query = function(){
+  function ready(){
+    refresh = range_query_refresh;
+    module_ready();
+  }
+  
+  function range_query_refresh(){
+    module_get(update, error);
+  }
+
+  function update(data){
+    console.log(data);
+    module_update(data);
+  }
+
+  function error(err){
+    console.error(err);
+  }
+
+  return {
+    update: update,
+    ready: ready
+  };
+}();
