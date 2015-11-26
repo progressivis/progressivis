742732af9af166025e0702002ade55b672d1c401
Romain Primet <romain.primet@inria.fr>
2015-11-26 15:27:25 +0100

    display range queries; now on to updates

--- progressivis/server/static/js/range_query.js
+++ progressivis/server/static/js/range_query.js
@@ -10,6 +10,25 @@ var range_query = function(){
 
   function update(data){
     module_update(data);
+
+    if(data.ranges === undefined){ return; }
+
+    //render sliders
+    var containers = d3.select("#sliders").selectAll(".slider").data(data.ranges);
+    var cEnter = containers.enter()
+                           .append("div")
+                           .attr("class", "slider");
+
+    cEnter.append("div")
+          .attr("class", "slider-container");
+
+    cEnter.append("div")
+          .attr("class", "slider-name")
+          .text(function(d){ return d.name; })
+          
+    //containers.call(d3.slider().value([10,40]).orientation("vertical"));
+    //the line above is simpler but does not work -- maybe because of a bug in d3-sliders. All thumbs are mixed up.
+    containers.selectAll(".slider-container").each(function(d,i){ d3.select(this).call(d3.slider().value([d.out_min,d.out_max]).axis(true).min(d.in_min).max(d.in_max).orientation("vertical")); });
   }
 
   function error(err){
@@ -17,7 +36,6 @@ var range_query = function(){
   }
 
   return {
-    update: update,
     ready: ready
   };
 }();
