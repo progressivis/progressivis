3cfe77805f08f6fe4db8b136f8d459e85281096f
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2015-12-15 16:29:35 -0500

    change sliders

--- progressivis/server/static/js/range_query.js
+++ progressivis/server/static/js/range_query.js
@@ -1,62 +1,90 @@
-var range_query = function(){
+var range_query = function() {
+    var sliders = {};
 
-  function ready(){
-    refresh = range_query_refresh;
-    module_ready();
-  }
-  
-  function range_query_refresh(){
-    module_get(update, error);
-  }
+    function ready(){
+        refresh = range_query_refresh;
+        module_ready();
+    }
+    
+    function range_query_refresh(){
+        module_get(update, error);
+    }
 
-  function update(data){
-    module_update(data);
+    function range_query_input(evt, value, d){
+        //set module inputs
+        var min = {};
+        min[d.name] = value[0];
+        var max = {};
+        max[d.name] = value[1];
+        module_input(min, update_success, error, module_id+"/min_value");
+        module_input(max, update_success, error, module_id+"/max_value");
+    }
 
-    if(data.ranges === undefined){ return; }
+    function range_query_create_slider(d, i) { 
+        var slider = d3.slider()
+                  .value([d.out_min,d.out_max])
+                  .axis(true)
+                  .min(d.in_min)
+                  .max(d.in_max)
+                  .orientation("horizontal")
+                  .on('slideend', function(evt, value) { range_query_input(evt, value, d); });
+        sliders[d.name] = slider; // store the slider to reuse it later
+        d3.select(this)
+            .call(slider);
+    }
 
-    //render sliders
-    var containers = d3.select("#sliders").selectAll(".slider").data(data.ranges);
-    var cEnter = containers.enter()
-                           .append("div")
-                           .attr("class", "slider");
+    function range_query_update_slider(d, i) {
+        var slider = sliders[d.name];
+        slider.value([d.out_min, d.out_max]);
+    }
 
-    cEnter.append("div")
-          .attr("class", "slider-container");
+    function range_query_remove_slider(d, i) {
+        delete sliders[d.name];
+    }
 
-    cEnter.append("div")
-          .attr("class", "slider-name")
-          .text(function(d){ return d.name; });
-          
-    //containers.call(d3.slider().value([10,40]).orientation("vertical"));
-    //the line above is simpler but does not work -- maybe because of a bug in d3-sliders. All thumbs are mixed up.
-    cEnter.selectAll(".slider-container")
-              .each(function(d,i){ 
-                      d3.select(this)
-                        .call(d3.slider()
-                                .value([d.out_min,d.out_max])
-                                .axis(true)
-                                .min(d.in_min)
-                                .max(d.in_max)
-                                .orientation("vertical")
-                                .on('slideend', function(evt, value){
-                                  //set module inputs
-                                  var min = {};
-                                  min[d.name] = value[0];
-                                  var max = {};
-                                  max[d.name] = value[1];
-                                  module_input(min, update_success, error, module_id+"/min_value");
-                                  module_input(max, update_success, error, module_id+"/max_value");
-                                })); 
-              });
-  }
+    function update(data){
+        module_update(data);
 
-  function update_success(){}
+        if(data.ranges === undefined) { return; }
 
-  function error(err){
-    console.error(err);
-  }
+        //render sliders
+        var containers = d3
+                .select("#sliders")
+                .selectAll(".slider")
+                .data(data.ranges, function(d) { return d.name; });
+        var cEnter = containers.enter()
+                .append("div")
+                .attr("class", "slider panel panel-default");
 
-  return {
-    ready: ready
-  };
+        cEnter.append("div")
+            .attr("class", "slider-name panel-body")
+            .text(function(d) { return "Attribute: '"+d.name+"'"; });
+        
+        cEnter.append("div")
+            .attr("class", "slider-container");
+
+        //containers.call(d3.slider().value([10,40]).orientation("vertical"));
+        //the line above is simpler but does not work -- maybe because of a bug in d3-sliders. All thumbs are mixed up.
+        cEnter.selectAll(".slider-container")
+            .each(range_query_create_slider);
+
+        // Update function
+        containers.selectAll(".slider-container")
+            .each(range_query_update_slider);
+
+        // Exit function
+        containers.exit()
+            .each(range_query_remove_slider)
+            .remove();
+    }
+
+    function update_success(){}
+
+    function error(err){
+        console.error(err);
+    }
+
+    return {
+        ready: ready
+    };
 }();
