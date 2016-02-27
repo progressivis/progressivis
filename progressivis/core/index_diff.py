818aabea7e659ac18b54831722f652f70c52557f
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2016-02-27 12:53:24 +0100

    test index diffs

--- progressivis/core/index_diff.py
+++ progressivis/core/index_diff.py
@@ -1,22 +1,25 @@
 from collections import namedtuple
 import pandas as pd
 
+NIL_INDEX = pd.Index([])
+
 index_diff = namedtuple('index_diff','created,updated,deleted')
 
 if pd.__version__ > '0.18':
     def index_difference(index1,index2):
-        #for now... let's optimize later
-        return index1.difference(index2)
-        # if not isinstance(index1,pd.RangeIndex) or not isinstance(index2,pd.RangeIndex) or \
-        #   index1._step != 1 or index2._step != 1:
-        #     return index1.difference(index2)
-        # if index1._stop < index2._start or index1._start > index2._stop:
-        #     return pd.RangeIndex(0,0)
-        # if index1._start < index2._start:
-        #     return pd.RangeIndex(index1._start, index2._start)
-        # if index1._stop > index2._stop:
-        #     return pd.RangeIndex(index2._stop, index1._stop)
-        # return index1.difference(index2)
+        if not isinstance(index1,pd.RangeIndex) \
+          or not isinstance(index2,pd.RangeIndex) \
+          or index1._step != 1 or index2._step != 1:
+            return index1.difference(index2)
+        if index1._stop < index2._start or index2._stop < index1._start:
+            return index1
+        if index1._start <= index2._start:
+            if index1._stop <= index2._stop:
+                return pd.RangeIndex(index1._start, index2._start)
+            return index1.difference(index2)
+        if index1._stop >= index2._stop:
+            return pd.RangeIndex(index2._stop, index1._stop)
+        return NIL_INDEX
 else:
     def index_difference(index1,index2):
         return index1.difference(index2)
--- tests/test_index_diff.py
+++ tests/test_index_diff.py
@@ -11,15 +11,29 @@ def random_range():
     return pd.RangeIndex(start, end)
 
 class TestIndexDiff(unittest.TestCase):
+    def do_test_diff(self, i1,i2,expect1=None,expect2=None):
+        d=index_difference(i1,i2)
+        if expect1 is None:
+            expect1=i1.difference(i2)
+        if not d.equals(expect1):
+            print i1, i2
+            self.assertTrue(False)
+        d=index_difference(i2,i1)
+        if expect2 is None:
+            expect2=i2.difference(i1)
+        if not d.equals(expect2):
+            print i2, i1
+            self.assertTrue(False)
+    
     def test_index_difference(self):
-        i1 = pd.RangeIndex(0,100)
-        i2 = pd.RangeIndex(10,110)
-
-        self.assertTrue(index_difference(i1,i2).equals(pd.RangeIndex(0,10)))
-        self.assertTrue(index_difference(i2,i1).equals(pd.RangeIndex(100,110)))
+        self.do_test_diff(pd.RangeIndex(62,106), pd.RangeIndex(62,106), NIL_INDEX, NIL_INDEX)
+        self.do_test_diff(pd.RangeIndex(62,106), pd.RangeIndex(65,89))
+        self.do_test_diff(pd.RangeIndex(84,238), pd.RangeIndex(3,52))
+        self.do_test_diff(pd.RangeIndex(0,100), pd.RangeIndex(10,110))
 
     def test_index_difference2(self):
         for i in range(1000):
             i1 = random_range()
             i2 = random_range()
-            self.assertTrue((index_difference(i1,i2)==i1.difference(i2)).all())
+            self.do_test_diff(i1,i2)
+
