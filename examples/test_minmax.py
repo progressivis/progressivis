da33084d00848d9aca1c4e7f26e60841c1e3bcbc
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2016-02-29 13:52:44 +0100

    fix test

--- examples/test_minmax.py
+++ examples/test_minmax.py
@@ -12,16 +12,16 @@ try:
     s = scheduler
 except:
     s = Scheduler()
-csv = CSVLoader('../nyc-taxi/yellow_tripdata_2014-10.csv.bz2', index_col=False,skipinitialspace=True,usecols=['pickup_longitude', 'pickup_latitude'], filter=filter, engine='c',scheduler=s)
-#csv = RandomTable(columns=['pickup_longitude', 'pickup_latitude'],rows=1000000, scheduler=s)
+#csv = CSVLoader('../nyc-taxi/yellow_tripdata_2015-10.csv.bz2', index_col=False,skipinitialspace=True,usecols=['pickup_longitude', 'pickup_latitude'], filter=filter, engine='c',scheduler=s)
+csv = RandomTable(columns=['pickup_longitude', 'pickup_latitude'],rows=1000000, throttle=100000, scheduler=s)
 long=Stats('pickup_longitude',scheduler=s)
 long.input.df = csv.output.df
 lat=Stats('pickup_latitude',scheduler=s)
 lat.input.df = csv.output.df
 pr = Print(scheduler=s)
-pr.input.inp = lat.output.stats
+pr.input.df = lat.output.stats
 prlen = Every(scheduler=s)
-prlen.input.inp = csv.output.df
+prlen.input.df = csv.output.df
 
 # if __name__=='__main__':
 #     print "Starting"
