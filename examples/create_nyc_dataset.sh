699df796f7e257cd2671ab5d590dfb32cefb1e48
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2016-02-29 14:16:39 +0100

    add script to download nyc taxi datasets

---
+++ examples/create_nyc_dataset.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+
+if [ \! -d examples ]; then
+    echo 'Should call the script from the main progressivis directory (with example/ as subdirectory)'
+    exit 1
+fi
+if [ \! -d ../nyc-taxi ]; then
+    mkdir ../nyc-taxi
+fi
+
+cd ../nyc-taxi
+for n in 01 02 03 04 05 06 07 08 09 10 11 12
+do
+    if [ \! -f 'green_tripdata_2015-'$n'.csv.bz2' ]; then
+        wget -nc 'https://storage.googleapis.com/tlc-trip-data/2015/green_tripdata_2015-'$n'.csv'
+        bzip2 'green_tripdata_2015-'$n'.csv' &
+    fi
+done
+
+for n in 01 02 03 04 05 06 07 08 09 10 11 12
+do
+    if [ \! -f 'yellow_tripdata_2015-'$n'.csv.bz2' ]; then
+        wget -nc 'https://storage.googleapis.com/tlc-trip-data/2015/yellow_tripdata_2015-'$n'.csv'
+        bzip2 'yellow_tripdata_2015-'$n'.csv' &
+    fi
+done
+
+for n in apr may jun jul aug sep
+do
+    if [ \! -f 'uber-raw-data-'$n'14.csv.bz2' ]; then
+        wget -nc 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-'$n'14.csv'
+        bzip2 'uber-raw-data-'$n'14.csv' &
+    fi
+done
