fabecd72e7d330d848b0bb7e4ad0ad902c369b86
Jean-Daniel Fekete <Jean-Daniel.Fekete@inria.fr>
2016-04-04 13:40:00 +0200

    fixes

--- progressivis/io/__init__.py
+++ progressivis/io/__init__.py
@@ -2,8 +2,10 @@ from .csv_loader import CSVLoader
 from .vec_loader import VECLoader
 from .input import Input
 from .variable import Variable
+from .add_to_row import AddToRow
 
 __all__ = ['CSVLoader',
            'VECLoader',
            'Input',
-           'Variable']
+           'Variable',
+           'AddToRow']
