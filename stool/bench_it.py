fe158265c9c7322b2be9954122299567e92cbd0d
Christian Poli <christian.poli@inria.fr>
2018-03-02 15:43:32 +0100

    progressive bench tool inspired from Progressivis Tracer

---
+++ stool/bench_it.py
@@ -0,0 +1,74 @@
+import sys
+from functools import wraps
+import os
+import os.path
+import tempfile
+from sqlalchemy import (Table, Column, Integer, Float, String, Sequence, BLOB,
+                        MetaData, ForeignKey, create_engine, select,
+                        UniqueConstraint)
+import uuid
+import json
+import numpy.distutils.cpuinfo as cpuinfo
+import pandas as pd
+from .utils import RunStepBenchEnv
+
+    
+def bench_this(to_decorate, module, env):
+    """
+    bench decorator
+    """
+    def bench_decorator(to_decorate):
+        """
+        This is the actual decorator. It brings togerther the function to be
+        decorated and the decoration stuff
+        """
+        @wraps(to_decorate)
+        def bench_wrapper(*args, **kwargs):
+            """
+            This function is the decoration
+            run_step(self, run_number, step_size, howlong)
+            """
+            #import pdb;pdb.set_trace()
+            run_number = args[0] if len(args)>0 else kwargs['run_number']
+            step_size = args[1] if len(args)>1 else kwargs['step_size']
+            howlong = args[2] if len(args)>2 else kwargs['howlong']
+            ut, st, cut, cst, et = os.times()
+            ret = to_decorate(*args, **kwargs) 
+            ut_, st_, cut_, cst_, et_ = os.times()
+            elapsed_time = et_ - et
+            sys_time = st_ -st
+            user_time = ut_ - ut
+            ld_avg_1, ld_avg_5, ld_avg_15 = os.getloadavg()
+            tbl = env.bench_table
+            ins = tbl.insert().values(module_id=module.id,
+                                          elapsed_time=elapsed_time,
+                                          sys_time=sys_time,
+                                          user_time=user_time,
+                                          run_number=run_number,
+                                          step_size=step_size,
+                                          howlong=howlong,
+                                          next_state=ret.get('next_state', 0),
+                                          steps_run=ret.get('steps_run', 0),
+                                          reads=ret.get('reads', 0),
+                                          updates=ret.get('updates', 0),
+                                          creates=ret.get('creates', 0)
+
+
+
+                                          )
+            with env.engine.connect() as conn:
+                conn.execute(ins)           
+            print(elapsed_time, sys_time, user_time)
+            return ret
+        return bench_wrapper
+    return bench_decorator(to_decorate)
+
+def decorate_module(m, env):
+    assert hasattr(m, 'run_step')
+    m.run_step = bench_this(to_decorate=m.run_step, module=m, env=env)
+def decorate(scheduler, db_name, drop_existing_db=False):
+    #import pdb;pdb.set_trace()
+    env = RunStepBenchEnv(db_name=db_name, drop_existing_db=drop_existing_db)
+    for m in scheduler.modules().values():
+        decorate_module(m, env)
+    return env
--- stool/utils.py
+++ stool/utils.py
@@ -2,11 +2,14 @@ import tempfile
 from sqlalchemy import (Table, Column, Integer, Float, String, Sequence, BLOB,
                         MetaData, ForeignKey, create_engine, select,
                         UniqueConstraint)
+import os
+import os.path
 import uuid
 import json
 import numpy.distutils.cpuinfo as cpuinfo
 import pandas as pd
-
+from abc import ABCMeta, abstractmethod, abstractproperty
+import six
 multi_processing = True # use False only for debug!
 
 if multi_processing:
@@ -47,19 +50,18 @@ def dump_table(table, db_name):
 
 
     
-class BenchEnv(object):
-    def __init__(self, db_name=None, append_mode=True):
+class BenchEnvBase(six.with_metaclass(ABCMeta, object)):
+    def __init__(self, db_name=None, append_mode=True, drop_existing_db=False):
         if db_name is None:
             _, self._db_name = tempfile.mkstemp(prefix='benchmarkit_', suffix='.db')
         else:
+            if drop_existing_db and os.path.isfile(db_name):
+                os.remove(db_name)
             self._db_name = db_name
         #import pdb; pdb.set_trace()
         self._engine = create_engine('sqlite:///' + self.db_name, echo=False)
         self._metadata = MetaData(bind=self._engine)
-        self._bench_tbl = None
-        self._case_tbl = None
-        self._measurement_tbl = None
-        self._append_mode = append_mode
+        self._append_mode = append_mode        
         self.create_tables_if()
 
     @property
@@ -70,6 +72,27 @@ class BenchEnv(object):
     def db_name(self):
         return self._db_name
 
+    @abstractmethod
+    def create_tables_if(self):
+        pass
+
+class BenchEnv(BenchEnvBase):
+    """
+    NB: BenchEnv and its subclasses are singletons
+    """
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(BenchEnv, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def __init__(self, db_name=None, append_mode=True, drop_existing_db=False):
+        self._bench_tbl = None
+        self._case_tbl = None
+        self._measurement_tbl = None
+        super(BenchEnv, self).__init(db_name, append_mode, drop_existing_db)
+        
     @property
     def bench_tbl(self):
         return self._bench_tbl
@@ -77,6 +100,7 @@ class BenchEnv(object):
     @property
     def measurement_tbl(self):
         return self._measurement_tbl
+
     @property
     def bench_list(self):
         tbl = self._bench_tbl
@@ -131,9 +155,41 @@ class BenchEnv(object):
         self._metadata.create_all(self._engine, checkfirst=self._append_mode)
 
 
+class RunStepBenchEnv(BenchEnvBase):
+    def __init__(self, db_name=None, append_mode=True, drop_existing_db=False):
+        self._run_step_bench_tbl = None
+        super(RunStepBenchEnv, self).__init__(db_name, append_mode, drop_existing_db)
+    def create_tables_if(self):
+        if 'run_step_bench_tbl' in self.engine.table_names():
+            self._run_step_bench_tbl = Table('run_step_bench_tbl', self._metadata, autoload=True)
+            return
+        self._run_step_bench_tbl = Table('run_step_bench_tbl', self._metadata,
+                                      Column('id', Integer, Sequence('m_id_seq'), primary_key=True),
+                                      Column('module_id', String),
+                                      Column('run_number', Integer),
+                                      Column('step_size', Integer),
+                                      Column('howlong', Integer),
+                                      Column('next_state', Integer),                                      
+                                      Column('steps_run', Integer),
+                                      Column('reads', Integer),
+                                      Column('updates', Integer),
+                                      Column('creates', Integer),                                      
+                                      Column('elapsed_time',  Float),
+                                      Column('sys_time',  Float),
+                                      Column('user_time',  Float),
+                                      Column('ld_avg_1',  Float),
+                                      Column('ld_avg_5',  Float),
+                                      Column('ld_avg_15',  Float),
+                                       autoload=False)
+    
+        self._metadata.create_all(self._engine, checkfirst=self._append_mode)
 
-
-
+    @property
+    def bench_table(self):
+        return self._run_step_bench_tbl
+    def dump(self):
+        dump_table(self.bench_table, self.db_name)
+        
 def get_random_name(prefix):
     return prefix+str(uuid.uuid4()).split('-')[-1]
 
