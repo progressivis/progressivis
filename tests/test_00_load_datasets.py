b548e63c33ccb77e70d81c82049d124d96051b5f
Christian Poli <christian.poli@inria.fr>
2018-09-19 11:16:31 +0200

    create missing files for the next tests

---
+++ tests/test_00_load_datasets.py
@@ -0,0 +1,32 @@
+from . import ProgressiveTest
+
+
+import logging
+import sys
+import six
+from progressivis.datasets import (get_dataset, get_dataset_bz2,
+                                       get_dataset_gz, get_dataset_lzma,
+                                       DATA_DIR)
+
+class TestLoadDatasets(ProgressiveTest):
+    def test_load_smallfile(self):
+        _ = get_dataset('smallfile')
+    def test_load_bigfile(self):        
+        _ = get_dataset('bigfile')
+    def test_load_smallfile_bz2(self):
+        _ = get_dataset_bz2('smallfile')
+    def test_load_bigfile_bz2(self):        
+        _ = get_dataset_bz2('bigfile')
+    def test_load_smallfile_gz(self):
+        _ = get_dataset_gz('smallfile')
+    def test_load_bigfile_gz(self):        
+        _ = get_dataset_gz('bigfile')
+    def test_load_smallfile_lzma(self):
+        if six.PY2:
+            return
+        _ = get_dataset_lzma('smallfile')
+    def test_load_bigfile_lzma(self):
+        if six.PY2:
+            return        
+        _ = get_dataset_lzma('bigfile')
+        
