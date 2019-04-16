5213180102d4a2887ac004efa46cc1da3743f6f0
Christian Poli <christian.poli@inria.fr>
2019-04-16 14:52:58 +0200

    improvement

--- progressivis/stats/kernel_density.py
+++ progressivis/stats/kernel_density.py
@@ -10,7 +10,9 @@ except:
     pass
 
 class KernelDensity(TableModule):
-    parameters = [('samples',  object, 1), ('bins',  np.dtype(int), 1),]
+    parameters = [('samples',  object, 1), ('bins',  np.dtype(int), 1),
+                      ('threshold',  np.dtype(int), 1000),
+                      ('knn',  np.dtype(int), 100),]
 
     def __init__(self, **kwds):
         self._add_slots(kwds,'input_descriptors',
@@ -19,6 +21,7 @@ class KernelDensity(TableModule):
         self._kde = None
         self._json_cache = {}
         self._inserted = 0
+        self._lately_inserted = 0        
         super(KernelDensity, self).__init__(**kwds)
     def run_step(self,run_number,step_size,howlong):
         dfslot = self.get_input_slot('table')
@@ -36,19 +39,24 @@ class KernelDensity(TableModule):
         if self._kde is None:
             self._kde = KNNKernelDensity(dfslot.data(), online=True)
         res = self._kde.run(steps)
-        self._inserted = res['numPointsInserted']
+        self._inserted += res['numPointsInserted']
+        self._lately_inserted += steps        
         samples = self.params.samples
-        sampleN = self.params.bins
-        scores = self._kde.score_samples(samples.astype(np.float32), k=100)
-        self._json_cache = {
-            'points': np.array(dfslot.data().loc[:500, :].to_dict(orient='split')['data']).tolist(),
-            'bins': sampleN,
-            'inserted': self._inserted,
-            'total': len(dfslot.data()),
-            'samples': [
-                (sample, score) for sample, score in zip(samples.tolist(), scores.tolist())
-            ]
-        }
+        sample_num = self.params.bins
+        threshold = self.params.threshold
+        knn = self.params.knn
+        if self._lately_inserted > threshold:
+            scores = self._kde.score_samples(samples.astype(np.float32), k=knn)
+            self._lately_inserted = 0
+            self._json_cache = {
+                'points': np.array(dfslot.data().loc[:500, :].to_dict(orient='split')['data']).tolist(),
+                'bins': sample_num,
+                'inserted': self._inserted,
+                'total': len(dfslot.data()),
+                'samples': [
+                    (sample, score) for sample, score in zip(samples.tolist(), scores.tolist())
+                ]
+            }
         return self._return_run_step(self.state_ready, steps_run=steps)
     def is_visualization(self):
         return True
