a34d5b52b2d5163b385371260838b4210fefa3bf
Christian Poli <Christian.Poli@inria.fr>
2020-04-14 18:22:01 +0200

    proc page size changes

--- progressivis/progressivis_nb_widgets/js/src/data_table.js
+++ progressivis/progressivis_nb_widgets/js/src/data_table.js
@@ -2,15 +2,24 @@ import {DataTable} from 'datatables';
 import $ from 'jquery';
 
 var data_table = null;
+
+function change_page(wobj, data_table){
+    var info = data_table.page.info();
+    info['draw'] = data_table.context[0].oAjaxData.draw + 1;
+    wobj.model.set("page", info);
+    console.log("info:", info)
+    wobj.touch();
+}
+
+
 function update_table(wobj, dt_id) {
     let data = wobj.model.get('data');
-    //let  data = JSON.parse(str_data);
     let columns_ = JSON.parse(wobj.model.get('columns'));
     console.log(data)
    
     console.log("dt_id:", dt_id);
     if ( ! $.fn.DataTable.isDataTable( '#'+dt_id ) ) {
-	console.log("DATATABLE CREATION!!!!!!!!!!!!!!!!!!!!!!!!");
+	console.log("Create DT:"+dt_id);
         var columns = columns_.map(function(c) { return {"sTitle": c.toString()}; });
         data_table = $( '#'+dt_id).DataTable( {
             "columns": columns,
@@ -19,15 +28,14 @@ function update_table(wobj, dt_id) {
             //"retrieve": true,
 	    "ajax": function (data_, callback, settings) {
 		var stuff = JSON.parse(wobj.model.get('data'));
-		if(stuff.draw< data_.draw) stuff.draw = data_.draw;
+		if(stuff.draw < data_.draw) stuff.draw = data_.draw;
 		callback(stuff);
 		
             }}).on( 'page.dt', function () {
-		var info = data_table.page.info();
-		info['draw'] = data_table.context[0].oAjaxData.draw + 1;
-		wobj.model.set("page", info);
-		wobj.touch();
-	    } );//window.my_table = data_table;
+		change_page(wobj, data_table);
+	    } ).on( 'length.dt', function ( e, settings, len ) {
+		change_page(wobj, data_table);
+	    });//window.my_table = data_table;
     } else {
         //$('#dataframe').dataTable({"retrieve": true}).ajax.reload();
         data_table.ajax.reload(null, false);
