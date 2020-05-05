304ca6dc337438b34a9ed54534965a0fce39e938
Christian Poli <Christian.Poli@inria.fr>
2020-05-05 11:33:33 +0200

    adapts for MCScatterPlot+asyncio

--- examples/test_mb_k_means.py
+++ examples/test_mb_k_means.py
@@ -1,7 +1,8 @@
 from progressivis import Scheduler, Print
 from progressivis.cluster import MBKMeans
 from progressivis.stats import RandomTable
-from progressivis.vis import ScatterPlot
+from progressivis.vis import MCScatterPlot
+import asyncio as aio
 
 try:
     s = scheduler
@@ -13,8 +14,11 @@ mbkmeans = MBKMeans(columns=['a', 'b'], n_clusters=8, batch_size=100, is_input=F
 mbkmeans.input.table = table.output.table
 prn = Print(scheduler=s)
 prn.input.df = mbkmeans.output.table
-sp = ScatterPlot('a', 'b', scheduler=s)
+sp = MCScatterPlot(scheduler=s, classes=[('Scatterplot', 'a', 'b')], approximate=True)
 sp.create_dependent_modules(mbkmeans,'table')
+sp['Scatterplot'].range_query_2d.hist_index_x.params.init_threshold = 1
+sp['Scatterplot'].range_query_2d.hist_index_y.params.init_threshold = 1
 
 if __name__ == '__main__':
-    table.start()
+    #table.start()
+    aio.run(s.start(coros=[aio.sleep(3600)]))
