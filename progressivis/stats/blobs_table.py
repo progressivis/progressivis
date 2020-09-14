05674ea6497bf216c9f9134faee7c5d5d3cc0f3a
Christian Poli <Christian.Poli@inria.fr>
2020-09-14 16:04:00 +0200

    adding a multivariate normal generator

--- progressivis/stats/blobs_table.py
+++ progressivis/stats/blobs_table.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 import logging
 
 import numpy as np
-
+from abc import ABCMeta, abstractmethod
 from ..utils.errors import ProgressiveError, ProgressiveStopIteration
 from progressivis import ProgressiveError, SlotDescriptor
 from ..table.module import TableModule
@@ -15,10 +15,26 @@ from ..table.constant import Constant
 from ..utils.psdict import PsDict
 from ..core.utils import integer_types
 from sklearn.datasets import make_blobs
+from sklearn.utils import shuffle as multi_shuffle
+
 logger = logging.getLogger(__name__)
 
 RESERVOIR_SIZE = 10000
 
+def make_mv_blobs(means, covs, n_samples, **kwds):
+    assert len(means) == len(covs)
+    n_blobs = len(means)
+    size = n_samples // n_blobs
+    blobs = []
+    labels = []
+    for i, (mean, cov) in enumerate(zip(means, covs)):
+        blobs.append(np.random.multivariate_normal(mean, cov, size, **kwds))
+        arr = np.empty(size, dtype='int64')
+        arr[:] = i
+        labels.append(arr)
+    blobs = np.concatenate(blobs)
+    labels = np.concatenate(labels)
+    return multi_shuffle(blobs, labels)
 
 def xy_to_dict(x, y, i, size, cols):
     res = {}
@@ -28,27 +44,28 @@ def xy_to_dict(x, y, i, size, cols):
     labs = y[i:] if k is None else y[i:k]
     return res, labs
 
-class BlobsTable(TableModule):
+class BlobsTableABC(TableModule):
     """Isotropic Gaussian blobs => table
     The purpose of the "reservoir" approach is to ensure the reproducibility of the results
     """
     outputs = [SlotDescriptor('labels', type=Table, required=False)]
+    kw_fun = None
     def __init__(self, columns, rows=-1, dtype='float64', seed=0, throttle=False, **kwds):
         super().__init__(**kwds)
-        self._kwds = self._filter_kwds(kwds, make_blobs)
-        assert 'centers' in self._kwds
+        self._kwds = self._filter_kwds(kwds, self.kw_fun)
+        """assert 'centers' in self._kwds
         assert 'n_samples' not in self._kwds
         assert 'n_features' not in self._kwds
-        assert 'random_state' not in self._kwds
+        assert 'random_state' not in self._kwds"""
         #self._kwds['n_samples'] = rows
         #self._kwds['n_features'] 
         self.default_step_size = 1000
         if isinstance(columns, integer_types):
             self.columns = ["_%d"%i for i in range(1, columns+1)]
-            self._kwds['n_features'] = columns
+            #self._kwds['n_features'] = columns
         elif isinstance(columns, (list, np.ndarray)):
             self.columns = columns
-            self._kwds['n_features'] = len(columns)
+            #self._kwds['n_features'] = len(columns)
         else:
             raise ProgressiveError('Invalid type for columns')
         self.rows = rows
@@ -95,11 +112,9 @@ class BlobsTable(TableModule):
             return self.labels()
         return super().get_data(name)
 
+    @abstractmethod
     def fill_reservoir(self):
-        X, y = make_blobs(n_samples=RESERVOIR_SIZE, random_state=self.seed, **self._kwds)
-        self.seed += 1
-        self._reservoir = (X, y)
-        self._reservoir_idx = 0
+        pass
 
     def run_step(self, run_number, step_size, howlong):
         if step_size == 0:
@@ -138,3 +153,34 @@ class BlobsTable(TableModule):
         else:
             next_state = self.state_ready
         return self._return_run_step(next_state, steps_run=step_size)
+
+class BlobsTable(BlobsTableABC):
+    kw_fun = make_blobs
+    def __init__(self, *args, **kwds):
+        #import pdb;pdb.set_trace()
+        super().__init__(*args, **kwds)
+        assert 'centers' in self._kwds
+        assert 'n_samples' not in self._kwds
+        assert 'n_features' not in self._kwds
+        assert 'random_state' not in self._kwds
+
+    def fill_reservoir(self):
+        X, y = make_blobs(n_samples=RESERVOIR_SIZE, random_state=self.seed, **self._kwds)
+        self.seed += 1
+        self._reservoir = (X, y)
+        self._reservoir_idx = 0
+
+class MVBlobsTable(BlobsTableABC):
+    kw_fun = make_mv_blobs
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.means = kwds['means']
+        self.covs = kwds['covs']
+
+    def fill_reservoir(self):
+        np.random.seed(self.seed)
+        X, y = make_mv_blobs(n_samples=RESERVOIR_SIZE, means=self.means, covs=self.covs, **self._kwds)
+        self.seed += 1
+        self._reservoir = (X, y)
+        self._reservoir_idx = 0
+    
--- tests/test_03_blobs_table.py
+++ tests/test_03_blobs_table.py
@@ -1,7 +1,7 @@
 from . import ProgressiveTest
 from progressivis.core import aio
 from progressivis import Every
-from progressivis.stats.blobs_table import BlobsTable
+from progressivis.stats.blobs_table import BlobsTable, MVBlobsTable
 from progressivis.stats.random_table import RandomTable
 from progressivis.linalg import Add
 import numpy as np
@@ -29,9 +29,45 @@ class TestBlobsTable(ProgressiveTest):
         sz = 100000
         centers = [(0.1, 0.3), (0.7, 0.5), (-0.4, -0.3)]
         blob1=BlobsTable(['a', 'b'], centers=centers,  cluster_std=0.2, rows=sz, scheduler=s)
-        #blob1.default_step_size = 1500
+        blob1.default_step_size = 1500
         blob2=BlobsTable(['a', 'b'], centers=centers,  cluster_std=0.2, rows=sz, scheduler=s)
-        #blob2.default_step_size = 200
+        blob2.default_step_size = 200
+        add = Add(scheduler=s)
+        add.input.first = blob1.output.table
+        add.input.second = blob2.output.table        
+        prlen = Every(proc=self.terse, constant_time=True, scheduler=s)
+        prlen.input.df = add.output.table
+        aio.run(s.start())
+        #s.join()
+        self.assertEqual(len(blob1.table()), sz)
+        self.assertEqual(len(blob2.table()), sz)
+        arr1 = blob1.table().to_array()
+        arr2 = blob2.table().to_array()
+        self.assertTrue(np.allclose(arr1, arr2))
+
+means = [0.1, 0.3], [0.7, 0.5], [-0.4, -0.3]
+covs = [[0.01, 0], [0, 0.09]], [[0.04, 0], [0, 0.01]], [[0.09, 0.04], [0.04, 0.02]]
+
+class TestMVBlobsTable(ProgressiveTest):
+    def test_mv_blobs_table(self):
+        s = self.scheduler()
+        module=MVBlobsTable(['a', 'b'], means=means, covs=covs, rows=10000, scheduler=s)
+        self.assertEqual(module.table().columns[0],'a')
+        self.assertEqual(module.table().columns[1],'b')
+        self.assertEqual(len(module.table().columns), 2) 
+        prlen = Every(proc=self.terse, constant_time=True, scheduler=s)
+        prlen.input.df = module.output.table
+        aio.run(s.start())
+        #s.join()
+        self.assertEqual(len(module.table()), 10000)
+
+    def test_mv_blobs_table2(self):
+        s = self.scheduler()
+        sz = 100000
+        blob1=MVBlobsTable(['a', 'b'], means=means, covs=covs, rows=sz, scheduler=s)
+        blob1.default_step_size = 1500
+        blob2=MVBlobsTable(['a', 'b'], means=means, covs=covs, rows=sz, scheduler=s)
+        blob2.default_step_size = 200
         add = Add(scheduler=s)
         add.input.first = blob1.output.table
         add.input.second = blob2.output.table        
