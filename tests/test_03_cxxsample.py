c27d5a6c98d784ce87011aed60ea495a1798cf8e
Christian Poli <Christian.Poli@inria.fr>
2021-03-12 07:31:18 +0100

    test for cxx_sample

---
+++ tests/test_03_cxxsample.py
@@ -0,0 +1,30 @@
+from . import ProgressiveTest
+
+from progressivis import Print
+from progressivis.io import CSVLoader
+from progressivis.stats import Sample
+from progressivis.datasets import get_dataset
+from progressivis.core import aio
+from progressivis.stats.cxxsample import Sample, CxxSample
+
+def print_repr(x):
+    print(repr(x))
+
+
+class TestCxxSample(ProgressiveTest):
+#    def setUp(self):
+#        log_level(logging.INFO)
+    @skipIf(CxxSample is None, "C++ module is missing")
+    def test_sample(self):
+        s = self.scheduler()
+        csv = CSVLoader(get_dataset('bigfile'), index_col=False, header=None, scheduler=s)
+        smp = Sample(samples=10,scheduler=s)
+        smp.input[0] = csv.output.result
+        prt = Print(proc=self.terse, scheduler=s)
+        prt.input[0] = smp.output.result
+        aio.run(csv.scheduler().start())
+        #print(repr(smp.result))
+        self.assertEqual(len(smp.result), 10)
+
+if __name__ == '__main__':
+    ProgressiveTest.main()
