43d26f6ecb0ffa40967709df0d183dbb4e3612c6
Christian Poli <Christian.Poli@inria.fr>
2021-03-12 09:40:38 +0100

    adding cxxsample.py

---
+++ progressivis/stats/cxxsample.py
@@ -0,0 +1,34 @@
+
+from progressivis.table.module import TableModule
+from progressivis.table.table import Table
+from progressivis.core.slot import SlotDescriptor
+try:
+    from .cxx_sample import Sample as CxxSample
+except:
+    CxxSample = None
+
+import numpy as np
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Sample(TableModule):
+    parameters = [('history', np.dtype(int), 3)]
+    inputs = [SlotDescriptor('table', type=Table, required=True)]
+
+    def __init__(self, columns=None, **kwds):
+        super().__init__(**kwds)
+        self._columns = columns
+        self.default_step_size = 10000
+        self.cxx_module = CxxSample(self)
+
+
+
+    def is_ready(self):
+        if self.get_input_slot('table').created.any():
+            return True
+        return super().is_ready()
+
+    def run_step(self, run_number, step_size, howlong):
+        return self.cxx_module.run(run_number, step_size, howlong)
