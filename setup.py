12fbac08aa1627bb04454e229b040840376cea03
Christian Poli <Christian.Poli@inria.fr>
2021-03-17 16:56:10 +0100

    fix

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ from setuptools.command.build_ext import build_ext
 #import numpy as np
 
 CONDA_PREFIX = os.getenv('CONDA_PREFIX')
-TRAVIS = os.getenv('USER') == 'travis'
-PROGRESSIVIS_CXX = TRAVIS or not CONDA_PREFIX
+MYBINDER = os.getenv('USER') == 'jovyan'
+WITH_CXX = not MYBINDER
 
 
 
@@ -150,7 +150,7 @@ setup(
         "aiohttp_jinja2",
         "python_socketio", "click"],
     # "pptable",
-    setup_requires= ['cython', 'numpy', 'pybind11', 'nose>=1.3.7', 'coverage'] if PROGRESSIVIS_CXX else [],
+    setup_requires= ['cython', 'numpy', 'pybind11', 'nose>=1.3.7', 'coverage'] if WITH_CXX else [],
     # test_suite='tests',
     test_suite='nose.collector',
     cmdclass=versioneer.get_cmdclass({'bench': RunBench}),
