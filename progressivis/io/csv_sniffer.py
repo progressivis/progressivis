38853ab65d2e53f67c6ea73e5b5369e02ba2a0eb
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2021-04-22 15:15:29 +0200

    Change types, usecols

--- progressivis/io/csv_sniffer.py
+++ progressivis/io/csv_sniffer.py
@@ -2,6 +2,7 @@ import csv
 import inspect
 import io
 import logging
+import pprint
 
 import pandas as pd
 import fsspec
@@ -86,13 +87,16 @@ class CSVSniffer:
         self._head = ""
         self._dialect = None
         self._df = None
+        self._df2 = None
         self._rename = None
-        self._usecols = None
+        self._types = None
         layout = widgets.Layout(border='solid')
         self.head_text = widgets.HTML()
         self.df_text = widgets.HTML()
-        self.tab = widgets.Tab([self.head_text, self.df_text])
-        for i, title in enumerate(["Head", "DataFrame"]):
+        self.df2_text = widgets.HTML()
+        self.error_msg = widgets.Textarea(description='Error:')
+        self.tab = widgets.Tab([self.head_text, self.df_text, self.df2_text])
+        for i, title in enumerate(["Head", "DataFrame", "DataFrame2"]):
             self.tab.set_title(i, title)
         self.delimiter = widgets.RadioButtons(
             options=list(zip(self.delimiters, self.del_values)))
@@ -120,10 +124,16 @@ class CSVSniffer:
                 ],
                 layout=layout),
             self.details])
-        self.cmdline = widgets.Text(description="Comand Line:",
-                                    # disabled=True,
-                                    layout=widgets.Layout(width="100%"))
-        self.box = widgets.VBox([self.top, self.cmdline, self.tab])
+        self.cmdline = widgets.Textarea(layout=widgets.Layout(width="100%"),
+                                        rows=3)
+        self.testBtn = widgets.Button(description="Test")
+        self.box = widgets.VBox([
+            self.top,
+            widgets.HBox([self.testBtn,
+                          widgets.Label(value="CmdLine:"),
+                          self.cmdline]),
+            self.tab])
+        self.testBtn.on_click(self.test_cmd)
         self.column_info = []
         self.clear()
         self.dataframe()
@@ -166,16 +176,21 @@ class CSVSniffer:
             raise ValueError(f"extra keywords arguments {args}")
 
     def set_cmdline(self):
-        cmdline = ""
-        defaults = self.signature.parameters
+        params = {}
         for key, val in self.params.items():
-            default = defaults[key].default
+            default = _parser_defaults[key]
             if val == default:
                 continue
-            if cmdline:
-                cmdline += ", "
-            cmdline += f"{key}={repr(val)}"
-        self.cmdline.value = cmdline
+            params[key] = val
+        self.cmdline.value = pprint.pformat(params)
+        # for key, val in self.params.items():
+        #     default = defaults[key].default
+        #     if val == default:
+        #         continue
+        #     if cmdline:
+        #         cmdline += ", "
+        #     cmdline += f"{key}={repr(val)}"
+        # self.cmdline.value = cmdline
 
     def clear(self):
         self.lines = 100
@@ -238,6 +253,21 @@ class CSVSniffer:
         self.dataframe_to_params()
         return self._df
 
+    def test_cmd(self, button):
+        strin = io.StringIO(self.head())
+        try:
+            self._df2 = pd.read_csv(strin, **self.params)
+        except ValueError as e:
+            self._df2 = None
+            self.df2_text.value = f'''
+<pre style="white-space: pre">Error {quote_html(repr(e))}</pre>
+'''
+        else:
+            with pd.option_context('display.max_rows', self.lines,
+                                   'display.max_columns', 0):
+                self.df2_text.value = self._df2._repr_html_()
+        self.tab.selected_index = 2
+
     def dataframe_to_params(self):
         df = self._df
         if df is None:
@@ -245,8 +275,6 @@ class CSVSniffer:
         # if self.params['names'] is None:
         #     self.params['names'] = list(df.columns)
         # # TODO test for existence?
-        # if self.params['usecols'] is None:
-        #     self.params['usecols'] = list(df.columns)
         self.set_cmdline()
 
     def dataframe_to_columns(self):
@@ -286,21 +314,50 @@ class CSVSniffer:
         names = [col for col in self._df.columns
                  if self.column[col].use.value]
         if names == list(self._df.columns):
-            self._usecols = None
+            del self.params['usecols']
         else:
-            self._usecols = names
+            self.params['usecols'] = names
+        self.set_cmdline()
+
+    def retype_columns(self):
+        types = {}
+        parse_dates = []
+        for name in list(self._df.columns):
+            col = self.column[name]
+            if col.use.value and col.default_type != col.retype.value:
+                type = col.retype.value
+                if type == "datetime":
+                    types[name] = "str"
+                    parse_dates.append(name)
+                else:
+                    types[name] = type
+        if types:
+            self._types = types
+            self.params['dtype'] = types
+        else:
+            self._types = None
+            del self.params['dtype']
+        if parse_dates:
+            self.params['parse_dates'] = parse_dates
+        else:
+            self.params['parse_dates'] = None
+        self.set_cmdline()
 
 
 class ColumnInfo:
-    numeric_types = ['int32', 'int64',
+    numeric_types = ['int8', 'uint8',
+                     'int16' 'uint16',
+                     'int32', 'int64',
                      'uint32', 'uint64',
-                     'float32', 'float64']
-    object_types = ['object', 'string',
-                    'categorical', 'datetime']
+                     'float32', 'float64',
+                     'str']
+    object_types = ['object', 'str',
+                    'category', 'datetime']
 
     def __init__(self, sniffer, series):
         self.sniffer = sniffer
         self.series = series
+        self.default_type = series.dtype.name
         self.name = widgets.Text(description="Column:",
                                  value=series.name,
                                  continuous_update=False,
@@ -308,7 +365,7 @@ class ColumnInfo:
         self.type = widgets.Text(description="Type:",
                                  value=series.dtype.name,
                                  disabled=True)
-        self.use = widgets.Checkbox(description="Use:",
+        self.use = widgets.Checkbox(description="Use",
                                     value=True)
         self.use.observe(self.usecols_column, names='value')
         self.rename = widgets.Text(description="Rename:",
@@ -317,9 +374,9 @@ class ColumnInfo:
         self.retype = widgets.Dropdown(description="Retype:",
                                        options=self.retype_values(),
                                        value=series.dtype.name)
+        self.retype.observe(self.retype_column, names='value')
         self.nunique = widgets.Text(description="Unique vals:",
-                                    value=str(series.nunique()),
-                                    disabled=True)
+                                    value=f"{series.nunique()}/{len(series)}")
         self.box = widgets.VBox([self.name, self.rename,
                                  self.type, self.retype,
                                  self.use, self.nunique,
@@ -338,3 +395,6 @@ class ColumnInfo:
 
     def usecols_column(self, change):
         self.sniffer.usecols_columns()
+
+    def retype_column(self, change):
+        self.sniffer.retype_columns()
