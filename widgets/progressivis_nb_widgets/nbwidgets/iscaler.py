bbf2dd0f609dcdd4f5006f10002baff63b2af541
Christian Poli <Christian.Poli@inria.fr>
2021-05-28 10:42:49 +0200

    saving work

--- widgets/progressivis_nb_widgets/nbwidgets/iscaler.py
+++ widgets/progressivis_nb_widgets/nbwidgets/iscaler.py
@@ -1,12 +1,140 @@
 import time
 import ipywidgets as ipw
+#import altair as alt
+import numpy as np
+import pandas as pd
 from .utils import update_widget
 from progressivis.core import asynchronize, aio
-        
+from IPython.display import display, clear_output
+#from altair_saver import save
+#import matplotlib.pyplot as plt
+from vega.widget import VegaWidget
+
+# "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+"""
+"selection": {"pts": {"type": "single", "encodings": ["x"]}},
+
+              "axis": {
+                  "labelAngle": 0,
+                  "labelOverlap": "parity"
+              }
+"""
+spec_no_data = {
+    "height": 500,
+    "width": 500,
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "mark": "bar",
+    "data": {
+        "name": "data"
+    },
+    "encoding": {
+        "x": {"type": "quantitative", "scale": {"domain": [0,127]},
+              "field": "bucket",
+        },
+        "y": {"type": "quantitative", "field": "level"},
+
+    }
+}
+
+
+
 def _refresh_info(wg):
     async def _coro(_1, _2):
         _ = _1, _2
         await asynchronize(wg.refresh_info)
+    return _coro
+
+
+def mpl_refresh_info_hist(hout, hmod):
+    #import pdb;pdb.set_trace()
+    #pass
+    if not hmod.result:
+        return
+    #with hout:
+    #print(hmod.result.last().to_dict())
+    #plt.clear()
+    hist = hmod.result.last().to_dict()['array']
+    bins = np.array(range(len(hist)+1))
+    width = 0.7 * (bins[1] - bins[0])
+    center = (bins[:-1] + bins[1:]) / 2
+
+    fig, ax = plt.subplots()#figsize=(8,3))
+    ax.bar(center, hist, align='center', width=width)
+    ax.set_xticks(bins)
+    with hout:
+        plt.show(fig)
+    #from io import BytesIO
+    #f = BytesIO()
+    #plt.savefig(f, format="png")
+    #wg = ipw.Image(value=f.getvalue(), width=512, height=512)
+    #with hout:
+    #    display(wg)
+    #hout.value=f.getvalue()
+    #print("width:", width)
+    #print("hist:", len(hist), hist)
+    #print("center:", len(center),center)
+
+def altair_refresh_info_hist(hout, hmod):
+    if not hmod.result:
+        return
+    hist = hmod.result.last().to_dict()['array']
+    bins = np.array(range(len(hist)+1))
+    width = 0.7 * (bins[1] - bins[0])
+    center = (bins[:-1] + bins[1:]) / 2
+    source = pd.DataFrame({
+        'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+        'b': [28, 55, 43, 91, 81, 53, 19, 87, 52]
+    })
+
+    c = alt.Chart(source).mark_bar().encode(
+        x='a',
+        y='b'
+    )
+    from io import BytesIO
+    f = BytesIO()
+    res = save(c, f, fmt='png')
+    assert res is None
+    assert isinstance(f.getvalue(), bytes)
+    hout.value=f.getvalue()
+    #with hout:
+    #    clear_output()
+    #    c.display()
+
+def refresh_info_hist(hout, hmod):
+    #import pdb;pdb.set_trace()
+    #pass
+    if not hmod.result:
+        return
+    spec_with_data = spec_no_data.copy()
+    res = hmod.result.last().to_dict()
+    hist = res['array']
+    min_ = res['min']
+    max_ = res['max']
+    ix = np.array(range(len(hist)))
+    #h = 10**(int(np.log10(np.max(hist)))+1)
+    bins = np.linspace(min_, max_, len(hist))
+    for i in [0,1]: # avoids blink
+        half_h = hist[i::2]
+        half_b = bins[i::2]
+        half_ix = ix[i::2]
+        remove_expr = '||'.join([f"datum.ix=={j}" for j in half_ix])
+        source = pd.DataFrame({
+            'bucket': half_ix,
+            'level': half_h,
+            #'ix': half_ix
+        })
+        spec_with_data["data"] = {
+            "name": "data",
+            "values": source.to_dict(orient='records'),
+        }
+        val = source.to_dict(orient='records')
+        hout.update('data', remove=remove_expr)
+        hout.update('data', insert=val)
+
+def _refresh_info_hist(hout, hmod):
+    async def _coro(_1, _2):
+        _ = _1, _2
+        await asynchronize(refresh_info_hist, hout, hmod)
     return _coro
 
 
@@ -89,10 +217,10 @@ class IScalerIn(ipw.GridBox):
             loop.create_task(m.control.from_input(values))
         return _cbk
 
-        
-class IScalerOut(ipw.GridBox):
+
+class IScalerOut(ipw.Tab):
     info_keys = {'clipped': 'Clipped:', 'ignored': 'Ignored:',
-                 'needs_changes': 'Needs changes:', 'has_buffered': 'Has buff:', 'last_reset': 'Last reset:'}    
+                 'needs_changes': 'Needs changes:', 'has_buffered': 'Has buff:', 'last_reset': 'Last reset:'}
     def __init__(self, module, scheduler=None):
         self._module = module
         self.info_labels = {}
@@ -104,7 +232,23 @@ class IScalerOut(ipw.GridBox):
         self._rows_label = ipw.Label('0')
         lst.append(self._rows_label)
         self._module.after_run_proc = _refresh_info(self)
-        super().__init__(lst, layout=ipw.Layout(grid_template_columns="repeat(2, 120px)"))
+        #super().__init__(lst, layout=ipw.Layout(grid_template_columns="repeat(2, 120px)"))
+        gb = ipw.GridBox(lst, layout=ipw.Layout(grid_template_columns="repeat(2, 120px)"))
+        if not module.hist:
+            super().__init__(children=[gb])
+            self.set_title(0, 'ScalerMinMax')
+        else:
+            hlist = [] #[ipw.Output() for _ in range(len(module.hist))]
+            for hmod in module.hist.values():
+                hout = VegaWidget(spec=spec_no_data) #ipw.Output() #ipw.Image(value=b'', width=512, height=512)
+                hlist.append(hout)
+                hmod.after_run_proc =  _refresh_info_hist(hout, hmod)
+            super().__init__(children=[gb, ipw.VBox(hlist)])
+            self.set_title(0, 'ScalerMinMax')
+            self.set_title(1, 'Histograms')
+
+
+
 
     def _info_label(self, k):
         v = ''
