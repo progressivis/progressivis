d8152eef7c3268b2a626459a92dd17edc368b78d
Christian Poli <Christian.Poli@inria.fr>
2021-11-28 16:34:19 +0100

    dev

--- progressivis/stats/kll.py
+++ progressivis/stats/kll.py
@@ -6,15 +6,17 @@ from ..core.slot import SlotDescriptor
 from ..utils.psdict import PsDict
 from ..core.decorators import process_slot, run_if_any
 from datasketches import kll_floats_sketch, kll_ints_sketch
+from ..core.utils import integer_types
+from collections.abc import Sequence
 import numpy as np
-
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 class KLLSketch(TableModule):
-    parameters = [('bins', np.dtype(int), 128),
-                  ('quantile', np.dtype(float), 0.5)]
+    parameters = [('binning', object, []),
+                  ('quantiles', object, [])]
     inputs = [SlotDescriptor('table', type=Table, required=True)]
 
     def __init__(self, column, k=200, **kwds):
@@ -56,19 +58,31 @@ class KLLSketch(TableModule):
                     raise ProgressiveError(f"Type {dtype_} of {self.column} "
                                            "is invalid for sketching")
                 self._kll = self._kll_func(self._k)
+            kll = self._kll                
             sk = self._kll_func(self._k)
             sk.update(column)
-            self._kll.merge(sk)
-            kll = self._kll
+            kll.merge(sk)
             max_ = kll.get_max_value()
             min_ = kll.get_min_value()
-            quantile = kll.get_quantile(self.params.quantile)
-            num_splits = self.params.bins
-            step = (max_ - min_) / num_splits
-            splits = [min_ + (i*step) for i in range(0, num_splits)]
-            pmf = kll.get_pmf(splits)
-            splits.append(max_)
-            res = dict(max=max_, min=min_, quantile=quantile, splits=splits, pmf=pmf)
+            quantiles = splits = pmf = []
+            if self.params.quantiles:
+                quantiles = kll.get_quantiles(self.params.quantiles)
+            if self.params.binning:
+                par_bin = self.params.binning
+                if isinstance(par_bin, integer_types):
+                    num_splits = par_bin
+                    splits = np.linspace(min_, max_, num_splits)
+                    pmf = kll.get_pmf(splits[:-1])
+                elif isinstance(par_bin, Sequence):
+                    splits = par_bin
+                    pmf = kll.get_pmf(splits)
+                elif isinstance(par_bin, dict):
+                    lower_ = par_bin['lower']
+                    upper_ = par_bin['upper']
+                    num_splits = par_bin['n_splits']
+                    splits = np.linspace(lower_, upper_, num_splits)
+                    pmf = kll.get_pmf(splits[:-1])
+            res = dict(max=max_, min=min_, quantiles=quantiles, splits=splits, pmf=pmf)
             if self.result is None:
                 self.result = PsDict(res) 
             else:
--- tests/test_03_kll.py
+++ tests/test_03_kll.py
@@ -7,12 +7,15 @@ from progressivis.stats import RandomTable
 import numpy as np
 from datasketches import kll_floats_sketch
 
-K = 200
+K = 300
 BINS = 128
-QUANTILE = 0.5
+QUANTILES = [0.3, 0.5, 0.7]
+SPLITS_SEQ = [0.3, 0.5, 0.7]
+
 
 class TestKll(ProgressiveTest):
     def test_kll(self):
+        np.random.seed(42)
         s = self.scheduler()
         random = RandomTable(3, rows=10_000, scheduler=s)
         kll=KLLSketch(column='_1', scheduler=s)
@@ -23,9 +26,71 @@ class TestKll(ProgressiveTest):
         val = random.result['_1'].value
         sk = kll_floats_sketch(K)
         sk.update(val)
+        print(kll.result['quantiles'])
         self.assertAlmostEqual(kll.result['max'], sk.get_max_value())
         self.assertAlmostEqual(kll.result['min'], sk.get_min_value())        
-        self.assertAlmostEqual(kll.result['quantile'], sk.get_quantile(QUANTILE))        
+        self.assertEqual(kll.result['quantiles'], [])
+        self.assertEqual(kll.result['splits'], [])
+        self.assertEqual(kll.result['pmf'], [])        
+
+    def test_kll2(self):
+        np.random.seed(42)
+        s = self.scheduler()
+        random = RandomTable(3, rows=10_000, scheduler=s)
+        kll=KLLSketch(column='_1', scheduler=s)
+        kll.params.quantiles = QUANTILES
+        kll.input[0] = random.output.result
+        pr=Print(proc=self.terse, scheduler=s)
+        pr.input[0] = kll.output.result
+        aio.run(s.start())
+        val = random.result['_1'].value
+        sk = kll_floats_sketch(K)
+        sk.update(val)
+        self.compare(kll.result['quantiles'], sk.get_quantiles(QUANTILES))
+
+    def test_kll3(self):
+        np.random.seed(42)
+        s = self.scheduler()
+        random = RandomTable(3, rows=10_000, scheduler=s)
+        kll=KLLSketch(column='_1', scheduler=s)
+        kll.params.binning = BINS
+        kll.input[0] = random.output.result
+        pr=Print(proc=self.terse, scheduler=s)
+        pr.input[0] = kll.output.result
+        aio.run(s.start())
+        val = random.result['_1'].value
+        sk = kll_floats_sketch(K)
+        sk.update(val)
+        max_ = sk.get_max_value()
+        min_ = sk.get_min_value()
+        num_splits = BINS
+        splits = np.linspace(min_, max_, num_splits)
+        pmf = sk.get_pmf(splits[:-1])
+        self.compare(kll.result['pmf'], pmf)
+
+    def test_kll4(self):
+        np.random.seed(42)
+        s = self.scheduler()
+        random = RandomTable(3, rows=10_000, scheduler=s)
+        kll=KLLSketch(column='_1', scheduler=s)
+        kll.params.binning = SPLITS_SEQ
+        kll.input[0] = random.output.result
+        pr=Print(proc=self.terse, scheduler=s)
+        pr.input[0] = kll.output.result
+        aio.run(s.start())
+        val = random.result['_1'].value
+        sk = kll_floats_sketch(K)
+        sk.update(val)
+        pmf = sk.get_pmf(SPLITS_SEQ)
+        self.compare(kll.result['pmf'], pmf)
+
+    def compare(self, res1, res2, atol=1e-02):
+        v1 = np.array(res1)
+        v2 = np.array(res2)
+        print(v1)
+        print(v2)
+        self.assertEqual(v1.shape, v2.shape)
+        self.assertTrue(np.allclose(v1, v2, atol=atol))
 
 
 if __name__ == '__main__':
