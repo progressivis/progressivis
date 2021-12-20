5ca4098efddc44472db48cbd78ea1da2fd8a93d6
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2021-12-20 21:42:50 +0100

    Fix mypy warning and misc storage

--- progressivis/storage/range.py
+++ progressivis/storage/range.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Union, Optional, Any, TYPE_CHECKING, cast, List
+
 from collections import Iterable
 import logging
 
@@ -8,6 +12,10 @@ from progressivis.core.bitmap import bitmap
 from .base import Dataset
 from .hierarchy import AttributeImpl
 
+
+if TYPE_CHECKING:
+    from .base import Shape, DTypeLike, ArrayLike, Attribute
+
 logger = logging.getLogger(__name__)
 
 
@@ -16,7 +24,14 @@ class RangeError(RuntimeError):
 
 
 class RangeDataset(Dataset):
-    def __init__(self, name, shape=None, dtype=None, data=None, **kwds):
+    def __init__(
+        self,
+        name: str,
+        shape: Optional[Shape] = None,
+        dtype: Optional[DTypeLike] = None,
+        data: Optional[Any] = None,
+        **kwds,
+    ):
         self._name = name
         if shape is None:
             if data is not None and hasattr(data, "shape"):
@@ -29,38 +44,38 @@ class RangeDataset(Dataset):
             )
         self._shape = shape
         if dtype is not None:
-            if dtype != np.int:
+            if dtype != np.int_:
                 raise TypeError("dtype of a RangeDataset should be integer")
-        self._dtype = np.int
+        self._dtype: np.dtype = np.dtype(np.int_)
         if kwds:
             logger.warning("Unused arguments %s", kwds)
         self._attrs = AttributeImpl()
 
     @property
-    def shape(self):
+    def shape(self) -> Shape:
         return self._shape
 
     @property
-    def dtype(self):
+    def dtype(self) -> np.dtype:
         return self._dtype
 
     @property
-    def maxshape(self):
+    def maxshape(self) -> Shape:
         return self._shape
 
     @property
-    def fillvalue(self):
+    def fillvalue(self) -> Any:
         return 0
 
     @property
-    def chunks(self):
+    def chunks(self) -> Shape:
         return self._shape
 
     @property
-    def size(self):
+    def size(self) -> int:
         return self._shape[0]
 
-    def resize(self, size, axis=None):
+    def resize(self, size: Union[int, ArrayLike], axis: int = None) -> None:
         if isinstance(size, tuple):
             if len(size) != 1:
                 raise KeyError(
@@ -71,7 +86,7 @@ class RangeDataset(Dataset):
             return
         self._shape = (size,)
 
-    def __getitem__(self, args):
+    def __getitem__(self, args) -> Any:
         if isinstance(args, tuple):
             if len(args) != 1:
                 raise KeyError("too many dimensions in __getitem__: %s", args)
@@ -82,17 +97,17 @@ class RangeDataset(Dataset):
             else:
                 raise IndexError("Index %d out of bounds for size %d", args, self.size)
         elif isinstance(args, np.ndarray):
-            if args.dtype == np.int:
+            if args.dtype == np.int_:
                 if (args >= self.size).any():
                     raise IndexError(
                         "Some index in %s out of bounds for size %d", args, self.size
                     )
                 return args
-            elif args.dtype == np.bool:
+            elif args.dtype == np.bool_:
                 return self[np.where(args)[0]]
         elif isinstance(args, Iterable):
             try:
-                count = len(args)
+                count = len(cast(List, args))
             # pylint: disable=bare-except
             except Exception:
                 count = -1
@@ -107,17 +122,17 @@ class RangeDataset(Dataset):
             return args
         raise KeyError("Invalid key for __getitem__: %s", args)
 
-    def __setitem__(self, args, val):
+    def __setitem__(self, args, val) -> None:
         if not np.array_equal(self[args], np.asarray(val)):
             raise RangeError("values incompatible with range")
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._shape[0]
 
     @property
-    def attrs(self):
+    def attrs(self) -> Attribute:
         return self._attrs
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
