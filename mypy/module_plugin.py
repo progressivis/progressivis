eb050692659fbdacfb0df3198ab8d4b149aec547
Christian Poli <Christian.Poli@inria.fr>
2023-03-09 10:14:48 +0100

    plugin improvement

--- mypy/module_plugin.py
+++ mypy/module_plugin.py
@@ -1,13 +1,15 @@
 from mypy.plugin import Plugin, ClassDefContext
 from sqlmypy import add_var_to_class, CB  # type: ignore
-from mypy.nodes import TypeInfo, NameExpr, CallExpr
+from mypy.nodes import TypeInfo, NameExpr, StrExpr, CallExpr
 from mypy.types import Instance, UnionType, NoneTyp
-from typing import Any, Union, cast
+from typing import Any, Union, Optional, List, cast
+
+Expr = Union[NameExpr, StrExpr]
 
 
-def get_full_name(name):
+def get_full_name(name: str) -> Optional[str]:
     if "." in name:
-        name = name.split(".")[:-1]
+        name = name.split(".")[-1]
     fullnames = dict(
         PTable="progressivis.table.table.PTable",
         BasePTable="progressivis.table.table_base.BasePTable",
@@ -19,7 +21,7 @@ def get_full_name(name):
     return fullnames.get(name)
 
 
-def get_content(obj):
+def get_content(obj: Expr) -> str:
     if isinstance(obj, NameExpr):
         return obj.name
     return obj.value
@@ -27,29 +29,31 @@ def get_content(obj):
 
 def decl_deco_hook(ctx: ClassDefContext) -> None:
     reason: CallExpr = cast(CallExpr, ctx.reason)
+    args: List[Expr] = cast(List[Expr], reason.args)
     if "custom_attr" in reason.arg_names:
         i = reason.arg_names.index("custom_attr")
-        if get_content(reason.args[i]) == "True":
+        if get_content(args[i]) == "True":
             return
     if "attr_name" in reason.arg_names:
         i = reason.arg_names.index("attr_name")
-        attr = get_content(reason.args[i])
+        attr = get_content(args[i])
     elif "name" in reason.arg_names:
         i = reason.arg_names.index("name")
-        attr = get_content(reason.args[i])
+        attr = get_content(args[i])
     else:
-        if not len(reason.args):
+        if not len(args):
             return  # fail
         if reason.arg_kinds[0].name != 'ARG_POS':
             return  # fail
-        attr = get_content(reason.args[0])
-    typ: Union[Instance, UnionType, NoneTyp, None] = None
+        attr = get_content(args[0])
+    typ: Union[Instance, UnionType, NoneTyp, str, None] = None
     if "type" in reason.arg_names:
         i = reason.arg_names.index("type")
-        typ = get_content(reason.args[i])
-    elif len(reason.args) > 1 and reason.arg_kinds[1].name == 'ARG_POS':
-        typ = get_content(reason.args[1])
+        typ = get_content(args[i])
+    elif len(args) > 1 and reason.arg_kinds[1].name == 'ARG_POS':
+        typ = get_content(args[1])
     if typ is not None:
+        assert isinstance(typ, str)
         fullname = get_full_name(typ)
         if fullname is None:
             return
