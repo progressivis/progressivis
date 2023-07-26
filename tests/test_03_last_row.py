bbd5c1ef0ea1137c4680ece0f01a036e5da0f2a2
Christian Poli <Christian.Poli@inria.fr>
2023-07-26 14:30:58 +0200

    removing JoinById

--- tests/test_03_last_row.py
+++ tests/test_03_last_row.py
@@ -2,14 +2,10 @@ from __future__ import annotations
 
 from . import ProgressiveTest
 
-from progressivis import Print, Every
+from progressivis import Every
 from progressivis.table.last_row import LastRow
-from progressivis.table.constant import Constant
 from progressivis.io import CSVLoader
 from progressivis.datasets import get_dataset
-from progressivis.table.table import PTable
-from progressivis.table.join_by_id import JoinById
-from progressivis.core.utils import get_random_name
 from progressivis.core import aio, notNone
 
 
@@ -29,27 +25,6 @@ class TestLastRow(ProgressiveTest):
         res = lr1.result
         assert res is not None
         self.assertEqual(res.at[0, "_1"], notNone(df.last())["_1"])
-
-    def test_last_row_simple(self) -> None:
-        s = self.scheduler()
-        t1 = PTable(name=get_random_name("cst1"), data={"xmin": [1], "xmax": [2]})
-        t2 = PTable(name=get_random_name("cst2"), data={"ymin": [3], "ymax": [4]})
-        cst1 = Constant(t1, scheduler=s)
-        cst2 = Constant(t2, scheduler=s)
-        join = JoinById(scheduler=s)
-        join.input[0] = cst1.output.result
-        join.input[0] = cst2.output.result
-        pr = Print(proc=self.terse, scheduler=s)
-        pr.input[0] = join.output.result
-        aio.run(s.start())
-        assert join.result is not None
-        last = notNone(join.result.last())
-        self.assertTrue(
-            last["xmin"] == 1
-            and last["xmax"] == 2
-            and last["ymin"] == 3
-            and last["ymax"] == 4
-        )
 
 
 if __name__ == "__main__":
