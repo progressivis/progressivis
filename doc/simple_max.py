7e555f57442b1430259aef72b9d6e07a7b6cf4b4
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2025-05-28 18:10:55 +0200

    Add new file

---
+++ doc/simple_max.py
@@ -0,0 +1,50 @@
+import numpy as np
+from typing import Any
+from progressivis import (
+    Module, ReturnRunStep, def_input, def_output,
+    PTable, PDict, document, process_slot, run_if_any
+)
+from ..core.utils import indices_len, fix_loc
+from progressivis.core.docstrings import INPUT_SEL
+
+
+@document
+@def_input("table", PTable, hint_type=Sequence[str], doc=INPUT_SEL)
+@def_output("result", PDict,
+            doc=("PDict where each key represents a column"))
+class SimpleMax(Module):
+    def __init__(self, **kwds: Any) -> None:
+        super().__init__(**kwds)
+        self.default_step_size = 10000
+
+    def run_step(
+        self, run_number: int, step_size: int, quantum: float
+    ) -> ReturnRunStep:
+        # Handle the changed input slots
+        table_slot = self.get_input_slot("table")
+        if table_slot.updated.any() or table_slot.deleted.any():
+            table_slot.reset()
+            table_slot.update(run_number)
+            self.reset()
+        # Extract the new chunk
+        indices = table_slot.created.next(length=step_size)
+        steps = indices_len(indices)
+        chunk = table_slot.data().loc[fix_loc(indices)]
+        # Apply the operation on the chunk
+        op = chunk.max(keepdims=False)
+        if self.result is None:
+            self.result = PDict(op)
+        else:
+            for k, v in self.result.items():
+                self.result[k] = np.fmax(op[k], v)
+        # Return the next state and number of steps handled
+        if table_slot.has_buffered():
+            next_state = Module.state_ready
+        else:
+            next_state = Module.state_blocked
+        return self._return_run_step(next_state, steps)
+
+    def reset(self) -> None:
+        if self.result is not None:
+            self.result.fill(-np.inf)
+
---
+++ progressivis/core/delta.py
@@ -0,0 +1,132 @@
+"""
+Changes (Delta) inside tables/columns/pintsets.
+"""
+from __future__ import annotations
+
+from typing import Optional, Any
+
+from ..core.pintset import PIntSet
+
+
+class Delta:
+    """
+    Delta is used to keep track of chages occuring in linear data
+    structures such as tables, columns, or pintsets.
+    """
+
+    def __init__(
+        self,
+        created: Optional[PIntSet] = None,
+        updated: Optional[PIntSet] = None,
+        deleted: Optional[PIntSet] = None,
+    ):
+        created = created if isinstance(created, PIntSet) else PIntSet(created)
+        updated = updated if isinstance(updated, PIntSet) else PIntSet(updated)
+        deleted = deleted if isinstance(deleted, PIntSet) else PIntSet(deleted)
+        self.created: PIntSet = created
+        self.updated: PIntSet = updated
+        self.deleted: PIntSet = deleted
+
+    def __repr__(self) -> str:
+        return "Delta(created=%s,updated=%s,deleted=%s)" % (
+            repr(self.created),
+            repr(self.updated),
+            repr(self.deleted),
+        )
+
+    def clear(self) -> None:
+        "Clear the changes"
+        self.created.clear()
+        self.updated.clear()
+        self.deleted.clear()
+
+    def test(self, verbose: bool = False) -> bool:
+        "Test if the Delta is valid"
+        b = (
+            bool(self.created & self.updated)
+            or bool(self.created & self.deleted)
+            or bool(self.updated & self.deleted)
+        )
+        if verbose and b:  # pragma no cover
+            print("self.created & self.updated", self.created & self.updated)
+            print("self.created & self.deleted", self.created & self.deleted)
+            print("self.updated & self.deleted", self.updated & self.deleted)
+        return not b
+
+    def add_created(self, bm: PIntSet) -> None:
+        "Add created items"
+        self.created.update(bm)
+        self.deleted -= bm
+        self.updated -= bm
+
+    def add_updated(self, bm: PIntSet) -> None:
+        "Add updated items"
+        self.updated.update(bm)
+        self.updated -= self.created
+        self.updated -= self.deleted
+
+    def add_deleted(self, bm: PIntSet) -> None:
+        "Add deleted items"
+        self.deleted.update(bm)
+        self.updated -= bm
+        self.created -= bm
+
+    def combine(
+        self,
+        other: Optional[Delta],
+        update_created: bool = True,
+        update_updated: bool = True,
+        update_deleted: bool = True,
+    ) -> None:
+        "Combine this Delta with another Delta"
+        if other is None:
+            return
+        if other.deleted:
+            # No need to expose created when they become deleted
+            created_deleted = self.created & other.deleted
+            if update_created:
+                self.created -= other.deleted
+            if update_updated:
+                self.updated -= other.deleted
+            if update_deleted:
+                self.deleted |= other.deleted
+                self.deleted -= created_deleted
+        if other.created:
+            # if re-created, pretend updated
+            created_deleted = other.created & self.deleted
+            if update_deleted:
+                self.deleted -= created_deleted
+            if update_created:
+                self.created |= other.created
+                self.created -= created_deleted
+            if update_updated:
+                self.updated |= created_deleted
+                self.updated -= self.created
+            # TODO handle case with created & updated -> created
+        if other.updated and update_updated:
+            created_updated = self.created & other.updated
+            self.updated |= other.updated - created_updated
+            self.updated -= other.created
+        assert self.test(verbose=True)  # test invariant
+
+    def __eq__(self, other: Any) -> bool:
+        return self is other or (
+            isinstance(other, Delta)
+            and self.created == other.created
+            and self.updated == other.updated
+            and self.deleted == other.deleted
+        )
+
+    def __ne__(self, other: Any) -> bool:
+        return not self == other
+
+    def copy(self) -> Delta:
+        "Copy this Indexupdate"
+        return Delta(
+            created=PIntSet(self.created),
+            updated=PIntSet(self.updated),
+            deleted=PIntSet(self.deleted),
+        )
+
+
+NIL_IU = Delta()
