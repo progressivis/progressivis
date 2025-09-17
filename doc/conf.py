482c541bb94fa54581e39664993681c609798d8e
Christian Poli <Christian.Poli@inria.fr>
2025-09-17 07:52:13 +0200

    rendr improved[skip ci]

--- doc/conf.py
+++ doc/conf.py
@@ -156,6 +156,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_js_files = ['jupyter-progressivis.js',]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
